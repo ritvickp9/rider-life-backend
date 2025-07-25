7c07ebcdf533c96feaf085a4e3ba0cc0533eb3db
Ritvick Pant <ritvickp9@gmail.com>
2025-07-25 16:43:42 +0530

    Cors addition

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
 	CORSMiddleware,
-	allow_origins=["http://localhost:3000"],  
+	allow_origins=["http://localhost:3000", "https://rider-life-nextjs.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"], 
